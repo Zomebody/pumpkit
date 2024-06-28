15dcc7b4c2a2ed86d03a4432c651455f983f4274
Zomebody <guido.de.vries@ziggo.nl>
2024-06-28 21:49:19 +0200

    network class almost ready

--- framework/modules/network.lua
+++ framework/modules/network.lua
@@ -43,15 +43,17 @@ function network:startServer(ip, slots)
 			local serverSocket = socket.tcp()
 			serverSocket:bind(ip, 0)
 			serverSocket:listen(1)
+			--serverSocket:setoption("tcp-nodelay", true)
 
 			local _, port = serverSocket:getsockname()
 			love.thread.getChannel("socket_port"):push(port)
 
 			local client = serverSocket:accept()
-			--client:settimeout(2)
 			local username = client:receive("*l") -- the first thing a client always sends is their username!
 			love.thread.getChannel("username" .. clientSlot):push(username)
 
+			--client:settimeout(0)
+			--serverSocket:settimeout(0)
 			while true do
 				-- check if you need to break out of the server loop
 				local event = love.thread.getChannel("alive" .. tostring(port)):pop()
@@ -61,23 +63,23 @@ function network:startServer(ip, slots)
 				end
 
 				-- keep receiving data from the client until there is no more data to read or too much data has been read temporarily
-				serverSocket:settimeout(2)
+				
 				local readData = nil
 				local counter = 0
+				client:settimeout(0.1)
 				repeat
 					counter = counter + 1
 					readData, err = client:receive("*l")
-					print(err)
+					
+					if readData ~= nil then
+						for word in string.gmatch(readData, "([^|]+)") do -- split string on a 'pipe' character
+							love.thread.getChannel("client_sent" .. clientSlot):push(word) -- push individual arguments into the queue
+						end
+						love.thread.getChannel("client_sent" .. clientSlot):push("|") -- signal the end of the data transmission from the client
+					end
 				until readData == nil or counter >= 10
 
-				if readData ~= nil then
-					print("readData:", readData)
-					for word in string.gmatch(readData, "([^|]+)") do -- split string on a 'pipe' character
-					print(word)
-						love.thread.getChannel("client_sent" .. clientSlot):push(word) -- push individual arguments into the queue
-					end
-					love.thread.getChannel("client_sent" .. clientSlot):push("|") -- signal the end of the data transmission from the client
-				end
+				
 
 				-- send any server event data over to the client
 				local sendData = nil
@@ -87,7 +89,6 @@ function network:startServer(ip, slots)
 						client:send(sendData .. string.char(10))
 					end
 				until sendData == nil
-				client:send(string.char(10)) -- send empty data to the client to inform them that no more data is being sent currently, to prevent them from yielding infinitely
 
 			end
 
@@ -106,14 +107,16 @@ function network:startServer(ip, slots)
 		
 		-- spawn a task that awaits until a client has connected to the socket in the thread
 		-- it keeps looping until the server goes offline, so even if a player disconnects it keeps going because a new player might join and fill in that spot
+		local nameInitialized = false
 		local t
 		t = task.spawn(
 			function()
-				-- while the player is connected on slot 'X', the usernameX channel will contain their username. One it's empty, the player disconnected
+				-- while the player is connected on slot 'X', the usernameX channel will contain their username. Once it's empty, the player disconnected
 				local username = love.thread.getChannel("username" .. slot):peek()
 				if username ~= nil then
 					Obj.ClientToName[i] = username
-				else
+					nameInitialized = true
+				elseif nameInitialized then
 					Obj.ClientToName[i] = nil
 					t:stop()
 				end
@@ -121,6 +124,8 @@ function network:startServer(ip, slots)
 			end,
 			0, math.huge, 0
 		)
+
+
 	end
 
 	setmetatable(Obj, server)
@@ -149,7 +154,7 @@ function network:connect(ip, port, username, callback)
 		local client = socket.tcp()
 
 		local success, err = client:connect(ip, port)
-		print("connected:", success)
+		
 		if success == 1 then
 			love.thread.getChannel("server_connect_status" .. tostring(totConAttempt)):push(true)
 		else
@@ -158,50 +163,120 @@ function network:connect(ip, port, username, callback)
 		end
 
 		client:setoption("tcp-nodelay", true)
-
-		client:send(username)
-
-		print("entering client while loop")
+		client:send(username .. string.char(10))
+		client:settimeout(0)
 
 		while true do
 			-- send any client event data over to the server
 			local sendData = nil
 			local disconnected = false
+			local counter = 0
 			repeat
+				counter = counter + 1
 				sendData = love.thread.getChannel("client_send"):pop()
 				if sendData ~= nil then
-					print("sendData:", sendData)
 					client:send(sendData .. string.char(10))
 					if sendData == "disconnect" then
 						disconnected = true
 					end
 				end
-			until sendData == nil or disconnected
+			until sendData == nil or disconnected or counter >= 10 -- stop when you're sending too much at once or when there's nothing to send or if you've disconnected
+
+
+			-- receive server event data that is sent to this client
+			counter = 0
+			repeat
+				counter = counter + 1
+				local serverData = client:receive("*l")
+				-- handle received server data
+				local isFirst = true
+				if serverData ~= nil then
+					if isFirst then
+						isFirst = false
+						if word == "disconnect" then
+							print("setting disconnected to true")
+							disconnected = true
+						end
+					end
+					for word in string.gmatch(serverData, "([^|]+)") do -- split string on a 'pipe' character
+						love.thread.getChannel("server_sent"):push(word) -- push individual arguments into the queue
+					end
+					love.thread.getChannel("server_sent"):push("|") -- signal the end of the data transmission from the client
+				end
+
+			until serverData == nil or counter >= 10 or disconnected
 
 			if disconnected then
 				client:close()
 				break
-			else
-				client:send(string.char(10))
 			end
 
 			
 		end
 	]=])
 
-	local t
-	t = task.spawn(
+	local connectTask
+	connectTask = task.spawn(
 		function()
 			local conStatus = love.thread.getChannel("server_connect_status" .. tostring(curAttempt)):pop()
 			if conStatus ~= nil then
 				callback(conStatus) -- either 'true' or a string with the error
-				t:stop()
+				connectTask:stop()
 			end
 			
 		end,
 		0, math.huge, 0
 	)
+
+	love.thread.getChannel("server_sent"):clear() -- just in case you previously connected to a server and there was some residue left behind or something
 	clientThread:start(ip, port, username, totalConnectionsAttempted)
+
+
+	local eventTask
+	eventTask = task.spawn(
+		function()
+			local disconnected = false
+			while love.thread.getChannel("server_sent"):peek() ~= nil do -- there's something in the channel, time to read it until the end-of-event argument is found
+				local ended = false
+				local item = nil
+				local args = {}
+				local isFirst = true
+				repeat
+					item = love.thread.getChannel("server_sent"):pop()
+					if isFirst and item ~= nil and item == "disconnect" then
+						disconnected = true
+						love.thread.getChannel("server_sent"):clear()
+						if Obj.Events["disconnect"] ~= nil then
+							connection.doEvents(Obj.Events["disconnect"])
+						end
+						break
+					end
+					isFirst = false
+					if item ~= nil then
+						if item == "|" and #args > 1 then -- #args should always be at least 2, but just in case it's no issue in putting this here
+							ended = true
+							local eventName = table.remove(args, 1)
+							if Obj.Events[eventName] ~= nil then
+								connection.doEvents(Obj.Events[eventName], unpack(args)) -- fire event on the client
+							end
+							-- clear arguments to prepare it for the next event
+							args = {}
+						else
+							args[#args + 1] = item
+						end
+						if item == "|" then
+							isFirst = true
+						end
+					end
+				until ended
+				if disconnected then
+					eventTask:stop()
+					break
+				end
+			end
+		end,
+		0, math.huge, 0
+	)
 
 	return setmetatable(Obj, client)
 end
@@ -231,7 +306,7 @@ function server:close()
 	end
 
 	-- TODO: for any remaining clients that are connected to the server, inform them that the server is closed
-	for i = 1, self.OpenPorts do
+	for i = 1, #self.OpenPorts do
 		love.thread.getChannel("alive" .. tostring(self.OpenPorts[i])):push(false) -- this will make the server threads close themselves and inform connected clients
 	end
 end
@@ -258,7 +333,7 @@ function server:broadcast(eventName, ...)
 	assert(type(eventName) == "string", "server:broadcast(eventName, ...) expects 'eventName' to be a string.")
 	assert(eventName:find("|") == nil, "server:broadcast(eventName, ...) expects 'eventName' to contain no '|' character.")
 	local args = {...}
-	local argsString = ""
+	local argsString = eventName .. "|"
 	for i = 1, #args do
 		assert(tostring(args[i]):find("|") == nil, "server:broacast(eventName, ...) expects all arguments to contain no '|' characters.")
 		if i ~= #args then
@@ -280,9 +355,10 @@ end
 function server:send(username, eventName, ...)
 	assert(type(username) == "string", "server:send(username, eventName, ...) expects 'username' to be a string.")
 	assert(type(eventName) == "string", "server:send(username, eventName, ...) expects 'eventName' to be a string.")
+	assert(eventName ~= "disconnect", "\"disconnect\" is a reserved event and cannot be used in server:send(username, eventName, ...).")
 	assert(eventName:find("|") == nil, "server:send(username, eventName, ...) expects 'eventName' to contain no '|' characters.")
 	local args = {...}
-	local argsString = ""
+	local argsString = eventName .. "|"
 	for i = 1, #args do
 		assert(tostring(args[i]):find("|") == nil, "server:send(username, eventName, ...) expects all arguments to contain no '|' character.")
 		if i ~= #args then
@@ -299,6 +375,18 @@ function server:send(username, eventName, ...)
 		end
 	end
 	return false
+end
+
+
+
+function server:kick(username)
+	for slot, uname in pairs(self.ClientToName) do
+		if uname == username then
+			local argsChannel = "server_send" .. tostring(slot)
+			love.thread.getChannel(argsChannel):push("disconnect")
+			return true
+		end
+	end
 end
 
 
@@ -321,7 +409,7 @@ function client:send(eventName, ...)
 	assert(eventName ~= "disconnect", "client:send(\"disconnect\") is a reserved event and cannot be used.")
 	assert(eventName:find("|") == nil, "client:send(eventName, ...) expects 'eventName' to contain no '|' characters.")
 	local args = {...}
-	local argsString = ""
+	local argsString = eventName .. "|"
 	for i = 1, #args do
 		assert(tostring(args[i]):find("|") == nil, "client:send(username, eventName, ...) expects all arguments to contain no '|' character.")
 		if i ~= #args then
@@ -330,7 +418,6 @@ function client:send(eventName, ...)
 			argsString = argsString .. tostring(args[i])
 		end
 	end
-	print(argsString)
 	love.thread.getChannel("client_send"):push(argsString)
 end
 
@@ -338,6 +425,9 @@ end
 
 function client:disconnect()
 	love.thread.getChannel("client_send"):push("disconnect")
+	if self.Events["disconnect"] ~= nil then
+		connection.doEvents(self.Events["disconnect"])
+	end
 end
 
 
@@ -353,7 +443,7 @@ love.update = function(...)
 		for slot, username in pairs(ActiveServers[i].ClientToName) do
 			-- empty the queue and call the server events
 			local argsChannel = "client_sent" .. tostring(slot)
-			while love.thread.getChannel(argsChannel):peek() ~= nil do -- there's something in the channel, time to read it until the end-of-event arguments is found
+			while love.thread.getChannel(argsChannel):peek() ~= nil do -- there's something in the channel, time to read it until the end-of-event argument is found
 				local ended = false
 				local item = nil
 				local args = {}
@@ -363,7 +453,9 @@ love.update = function(...)
 						if item == "|" and #args > 1 then -- #args should always be at least 2, but just in case it's no issue in putting this here
 							ended = true
 							local eventName = table.remove(args, 1)
-							connection.doEvents(ActiveServers[i].Events[eventName], username, unpack(args)) -- fire event on the server
+							if ActiveServers[i].Events[eventName] ~= nil then
+								connection.doEvents(ActiveServers[i].Events[eventName], username, unpack(args)) -- fire event on the server
+							end
 							-- clear arguments to prepare it for the next event
 							args = {}
 						else
