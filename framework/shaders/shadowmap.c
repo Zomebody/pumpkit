4735a2a3a067fdde6c22e2d8a0a57ca34b3487ea
Zomebody <guido.de.vries@ziggo.nl>
2025-07-13 14:41:47 +0200

    no more inverse() impl in shaders, glsl3 has it

--- framework/shaders/shadowmap.c
+++ framework/shaders/shadowmap.c
@@ -1,4 +1,4 @@
-
+#pragma language glsl3
 #ifdef VERTEX
 
 uniform mat4 sunWorldMatrix;
@@ -71,57 +71,6 @@ mat4 getTranslationMatrix(vec3 translation) {
 		translation.x, translation.y, translation.z, 1.0
 	);
 }
-
-
-// should be correct. Verify here: https://github.com/glslify/glsl-inverse/blob/master/index.glsl
-mat4 inverse(mat4 m) {
-	float
-		a00 = m[0][0], a01 = m[0][1], a02 = m[0][2], a03 = m[0][3],
-		a10 = m[1][0], a11 = m[1][1], a12 = m[1][2], a13 = m[1][3],
-		a20 = m[2][0], a21 = m[2][1], a22 = m[2][2], a23 = m[2][3],
-		a30 = m[3][0], a31 = m[3][1], a32 = m[3][2], a33 = m[3][3],
-
-	b00 = a00 * a11 - a01 * a10,
-	b01 = a00 * a12 - a02 * a10,
-	b02 = a00 * a13 - a03 * a10,
-	b03 = a01 * a12 - a02 * a11,
-	b04 = a01 * a13 - a03 * a11,
-	b05 = a02 * a13 - a03 * a12,
-	b06 = a20 * a31 - a21 * a30,
-	b07 = a20 * a32 - a22 * a30,
-	b08 = a20 * a33 - a23 * a30,
-	b09 = a21 * a32 - a22 * a31,
-	b10 = a21 * a33 - a23 * a31,
-	b11 = a22 * a33 - a23 * a32,
-
-	det = b00 * b11 - b01 * b10 + b02 * b09 + b03 * b08 - b04 * b07 + b05 * b06;
-
-	if (abs(det) < 0.00001) {
-		return mat4(1.0); // If determinant is 0, return identity matrix (or handle however you want)
-	}
-
-	float invDet = 1.0 / det;
-
-	return mat4(
-		(a11 * b11 - a12 * b10 + a13 * b09) * invDet,
-		(a02 * b10 - a01 * b11 - a03 * b09) * invDet,
-		(a31 * b05 - a32 * b04 + a33 * b03) * invDet,
-		(a22 * b04 - a21 * b05 - a23 * b03) * invDet,
-		(a12 * b08 - a10 * b11 - a13 * b07) * invDet,
-		(a00 * b11 - a02 * b08 + a03 * b07) * invDet,
-		(a32 * b02 - a30 * b05 - a33 * b01) * invDet,
-		(a20 * b05 - a22 * b02 + a23 * b01) * invDet,
-		(a10 * b10 - a11 * b08 + a13 * b06) * invDet,
-		(a01 * b08 - a00 * b10 - a03 * b06) * invDet,
-		(a30 * b04 - a31 * b02 + a33 * b00) * invDet,
-		(a21 * b02 - a20 * b04 - a23 * b00) * invDet,
-		(a11 * b07 - a10 * b09 - a12 * b06) * invDet,
-		(a00 * b09 - a01 * b07 + a02 * b06) * invDet,
-		(a31 * b01 - a30 * b03 - a32 * b00) * invDet,
-		(a20 * b03 - a21 * b01 + a22 * b00) * invDet
-	);
-}
-
 
 
 
